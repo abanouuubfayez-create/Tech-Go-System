a7a80108f95baa34d6303762999448160622b553
Abanoub Fayez <https://github.com/abanouuubfayez-create/School.git>
2026-07-20 13:18:48 +0300

    Use gemini-pro instead of gemini-1.5-flash-latest to avoid model not found errors

--- bump_version.py
+++ bump_version.py
@@ -1,7 +1,7 @@
 import re
 import os
 
-new_version = "?v=888888"
+new_version = "?v=999999"
 
 for filename in ['index.html', 'employee.html', 'login.html']:
     if not os.path.exists(filename):
@@ -9,10 +9,10 @@ for filename in ['index.html', 'employee.html', 'login.html']:
     with open(filename, 'r', encoding='utf-8') as f:
         content = f.read()
     
-    # Replace ?v=xxxxxx with ?v=888888
+    # Replace ?v=xxxxxx with ?v=999999
     content = re.sub(r'\?v=\d+', new_version, content)
     
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(content)
 
-print("Bumped version to 888888")
+print("Bumped version to 999999")
